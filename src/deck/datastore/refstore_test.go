3e444084575f917ea5c46390188223da89fa4d08
jeh727 <jeh727@gmail.com>
2016-10-06 19:35:08 -0400

    Added tests and benchmarks to store

--- src/deck/datastore/refstore_test.go
+++ src/deck/datastore/refstore_test.go
@@ -3,6 +3,7 @@ package datastore
 
 import(
     "deck/log"
+    "fmt"
     "testing"
 )
 
@@ -11,18 +12,68 @@ func TestRefStore(t *testing.T) {
     log.InitDefault()
 
     store := NewRefStore()
+    var ok bool
+    var data *string
     
-    if !store.Store("foo", 0, "bar") {
-        t.Error("Failed to store initial value foo:0")
+    topics := []string{"foo", "bar", "rah", "blah"}
+    
+    // Test over topics.  New topics should start empty
+    for _,topic := range(topics) {
+        
+        for storeIndex := int64(0); storeIndex < 10; storeIndex++ {
+            
+            for loadIndex := int64(0); loadIndex < 10; loadIndex++ {
+                data, ok = store.Load(topic, loadIndex)
+                if loadIndex < storeIndex {    
+                    if !ok {
+                        t.Error("Failed to load inserted value: ", loadIndex)
+                    }else if *data != fmt.Sprintf("%d", loadIndex) {
+                        t.Error("Loaded unexpected data: ", *data)
+                    }
+                } else if ok {
+                    t.Error("Unepectedly loaded value: ", loadIndex)
+                } 
+            }
+            
+            for storeIndex2 := int64(0); storeIndex2 < storeIndex; storeIndex2++ {
+                //Store
+                if store.Store(topic, storeIndex2, fmt.Sprintf("%d", storeIndex2)) {
+                    t.Error("Stored already inserted value: %d", storeIndex2)
+                }
+            }
+            
+            //Store
+            if !store.Store(topic, storeIndex, fmt.Sprintf("%d", storeIndex)) {
+                t.Error("Failed to store value: %d", storeIndex)
+            }
+        }
     }
-    
-    data, ok := store.Load("foo", 0)
-    if !ok {
-        t.Error("Failed to load inserted value foo:0")
-    }
-    
-    if *data != "bar" {
-        t.Error("Expected value bar not return from foo:0")
-    }
-    
 }
+
+func BenchmarkRefStoreStore(b *testing.B) {
+    log.InitDefault()
+
+    store := NewRefStore()
+    
+    // Store N items
+    for index := int64(0); index < int64(b.N); index++ {
+        store.Store("foo", index, fmt.Sprintf("%d", index))
+    }
+}
+
+func BenchmarkRefStoreLoad(b *testing.B) {
+    log.InitDefault()
+
+    store := NewRefStore()
+    
+    // Store N items
+    for index := int64(0); index < int64(100); index++ {
+        store.Store("foo", index, fmt.Sprintf("%d", index))
+    }
+    
+    // Load N items
+    for index := int64(0); index < int64(b.N); index++ {
+        store.Load("foo", index % 100)
+    }
+}
+    
